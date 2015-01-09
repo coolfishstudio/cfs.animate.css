81d348321ff5558f5b14f7e9594473095c39d57c
coolfish <461836324@qq.com>
2015-01-09 19:42:07 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	contentDisposition = require('content-disposition'),
+	destroy = require('destroy'),
+	onFinished = require('on-finished');
 
 var port = 9237;//端口号
+var filePath = '/CFS.Animate.min.css';//下载路径
+
 /*
 	这是一个简单的服务器 
 	主要作用是 启动这个静态文件
@@ -15,6 +20,14 @@ var server = http.createServer(function(req, res){
 		serve(__dirname + req.url, 'text/javascript');
 	}else if('GET' == req.method && ('/CFS.Animate.css' == req.url || '/CFS.Animate.min.css' == req.url)){//加载css
 		serve(__dirname + req.url, 'text/css');
+	}else if('GET' == req.method && '/download/CFS.Animate.min.css' == req.url){//下载css
+		res.setHeader('Content-Type', 'text/css')
+		res.setHeader('Content-Disposition', contentDisposition(__dirname + filePath));
+		var stream = fs.createReadStream(__dirname + filePath);
+		stream.pipe(res);
+		onFinished(res, function(err){
+			destroy(stream);
+		})
 	}else if('GET' == req.method && '/src/' == req.url.substr(0,5)){//加载源文件
 		if('.js' == getType(req.url)){
 			serve(__dirname + req.url, 'text/javascript');
