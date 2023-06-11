d4bda259f753230015898c9179bccee9777d9b0f
yves <461836324@qq.com>
2023-06-11 22:58:00 +0800

    兼容微前端

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var http = require('http'),
-	fs = require('fs'),
-	contentDisposition = require('content-disposition'),
-	destroy = require('destroy'),
-	onFinished = require('on-finished');
+	fs = require('fs');
 
-var port = 9237;//端口号
+var port = 3801;//端口号
 var filePath = '/CFS.Animate.min.css';//下载路径
 
 /*
-	这是一个简单的服务器 
+	这是一个简单的服务器
 	主要作用是 启动这个静态文件
 	只支持 加载css/js
 	默认跳转也面 只有首页
@@ -20,14 +17,6 @@ var server = http.createServer(function(req, res){
 		serve(__dirname + req.url, 'text/javascript');
 	}else if('GET' == req.method && ('/CFS.Animate.css' == req.url || '/CFS.Animate.min.css' == req.url)){//加载css
 		serve(__dirname + req.url, 'text/css');
-	}else if('GET' == req.method && '/download/CFS.Animate.min.css' == req.url){//下载css
-		res.setHeader('Content-Type', 'text/css')
-		res.setHeader('Content-Disposition', contentDisposition(__dirname + filePath));
-		var stream = fs.createReadStream(__dirname + filePath);
-		stream.pipe(res);
-		onFinished(res, function(err){
-			destroy(stream);
-		})
 	}else if('GET' == req.method && '/src/' == req.url.substr(0,5)){//加载源文件
 		if('.js' == getType(req.url)){
 			serve(__dirname + req.url, 'text/javascript');
@@ -42,7 +31,10 @@ var server = http.createServer(function(req, res){
 		res.end('Not found');
 	}
 	function serve(path, type){
-		res.writeHead(200, {'Content-Type': type});
+		res.writeHead(200, {
+			'Content-Type': type,
+			'Access-Control-Allow-Origin': '*'
+		});
 		fs.createReadStream(path).pipe(res);
 	}
 	function getType(url){
